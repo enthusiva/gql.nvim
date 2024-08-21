fdb617838ffda55691bd7d9a6947c9ab72f4de0b
Sivakumar Palanisamy <sivakumar@enthutech.in>
2024-08-21 16:45:57 +0530

    added plugin structure

--- lua/gql/init.lua
+++ lua/gql/init.lua
@@ -1,14 +1,14 @@
 -- Load the configuration
 local config = require("gql.config")
 
--- Load the keybindings
-require("gql.keybindings")
-
 -- Plugin setup function, to be called from your Neovim configuration
 local M = {}
 
 M.setup = function(user_config)
 	config.setup(user_config)
+
+	-- Load the keybindings
+	require("gql.keybindings")
 end
 
 return M
